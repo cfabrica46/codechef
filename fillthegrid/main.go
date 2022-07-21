7f9fa59afa83852b330525f206a715d86eb3b8d7
César Caycho <cfabrica46@gmail.com>
2022-07-20 23:21:04 -0500

    [FEAT]: added fill the grid

---
+++ fillthegrid/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	var attemps int
+
+	fmt.Scan(&attemps)
+
+	for i := 0; i < attemps; i++ {
+		fillTheGrid()
+	}
+}
+
+func fillTheGrid() {
+	in := bufio.NewReader(os.Stdin)
+
+	line, err := in.ReadString('\n')
+	if err != nil {
+		return
+	}
+
+	line = line[:len(line)-1]
+
+	values := strings.Split(line, " ")
+
+	n, err := strconv.Atoi(values[0])
+	if err != nil {
+		return
+	}
+
+	m, err := strconv.Atoi(values[1])
+	if err != nil {
+		return
+	}
+
+	switch {
+	case n%2 == 0 && m%2 == 0:
+		fmt.Println(0)
+	case n&2 != 0 && m%2 == 0:
+		fmt.Println(m)
+	case n&2 == 0 && m%2 != 0:
+		fmt.Println(n)
+	case n&2 != 0 && m%2 != 0:
+		fmt.Println(m + n - 1)
+	}
+}
